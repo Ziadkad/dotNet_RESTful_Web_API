3422eb71338388fd4862c244c39456fe6c693b86
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-06-01 18:44:42 +0100

    Adding Repositories and IRepositories

---
+++ Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using dotNet_RESTful_Web_API.models;
+
+namespace dotNet_RESTful_Web_API.Repository.IRepository;
+
+public interface IUserRepository
+{
+    Task<List<User>> GetAll(Expression<Func<User,bool>> filter = null);
+    Task<User> Get(Expression<Func<User,bool>>? filter, bool tracked = true);
+    Task Create(User entity);
+    Task Remove(User entity);
+    Task Save();
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using dotNet_RESTful_Web_API.Data;
+using dotNet_RESTful_Web_API.models;
+using dotNet_RESTful_Web_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNet_RESTful_Web_API.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _db;
+
+    public UserRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<User>> GetAll(Expression<Func<User,bool>> filter)
+    {
+        IQueryable<User> query = _db.Users;
+        if(filter != null){query.Where(filter);}
+        return await query.ToListAsync();
+    }
+
+    public async Task<User> Get(Expression<Func<User,bool>>? filter, bool tracked = true)
+    {
+        IQueryable<User> query = _db.Users;
+        if (!tracked) { query.AsNoTracking();}
+        if(filter != null){query.Where(filter);}
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task Create(User entity)
+    {
+        await _db.Users.AddAsync(entity);
+        await Save();
+    }
+
+    public async Task Remove(User entity)
+    {
+        _db.Users.Remove(entity);
+        await Save();
+    }
+
+    public async Task Save()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
